bbb820adceab6815219a2019f8a99e4d3484f12f
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-02-26 13:41:40 -0500

    checkpoint 4 -- week 2 lab

--- cp-lab-2/src/App.jsx
+++ cp-lab-2/src/App.jsx
@@ -6,6 +6,26 @@ const App = () => {
 
   const updateCount = () => setCount(count + multiplier);
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
@@ -24,17 +44,17 @@ const App = () => {
         <div className="upgrade">
           <h3>Double Stuffed 👯‍♀️</h3>
           <p>2x per click</p>
-          <button>10 samosas</button>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Party Pack 🎉</h3>
           <p>5x per click</p>
-          <button>100 samosas</button>
+          <button onClick={buyPartyPack}>100 samosas</button>
         </div>
         <div className="upgrade">
           <h3>Full Feast 👩🏽‍🍳</h3>
           <p>10x per click</p>
-          <button>1000 samosas</button>
+          <button onClick={buyFullFeast}>1000 samosas</button>
         </div>
       </div>
     </div>
