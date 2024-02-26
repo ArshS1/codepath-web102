83970fac62bfc10514ab2e9581b45dab1c72877f
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-02-26 13:35:52 -0500

    checkpoint 1 -- week 2 lab

--- cp-lab-2/src/App.jsx
+++ cp-lab-2/src/App.jsx
@@ -4,7 +4,11 @@ const App = () => {
 
   return (
     <div className="App">
-
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: </h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5nKnmskmIUk_Mu_DA6oRcmjy7eujkXmy1DUifz2MbmA&s" className='samosa' alt="" />
+      </div>
     </div>
   )
 }
