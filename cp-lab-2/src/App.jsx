e6ec0f0187dcb4612e80022d50500c49163471ed
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-02-26 13:36:49 -0500

    checkpoint 3 -- week 2 lab

--- cp-lab-2/src/App.jsx
+++ cp-lab-2/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css';
+import { useState } from 'react';
+
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+
+  const updateCount = () => setCount(count + multiplier);
+
 
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: </h2>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5nKnmskmIUk_Mu_DA6oRcmjy7eujkXmy1DUifz2MbmA&s" className='samosa' alt="" />
+        <h2>Count: {count}</h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5nKnmskmIUk_Mu_DA6oRcmjy7eujkXmy1DUifz2MbmA&s" className='samosa' onClick={updateCount} alt="samosa pic" />
       </div>
     </div>
   )
