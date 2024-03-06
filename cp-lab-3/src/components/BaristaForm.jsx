5a7e413f470c2d2718a783507267e9ae10760545
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-03-06 14:14:53 -0500

    checkpoint 2 - week 3 lab

--- cp-lab-3/src/components/BaristaForm.jsx
+++ cp-lab-3/src/components/BaristaForm.jsx
@@ -1,5 +1,19 @@
 import React, { Component, useState } from "react";
 const BaristaForm = () => {
+  const [inputs, setInputs] = useState({
+    temperature: "",
+    milk: "",
+    syrup: "",
+    blended: "",
+  });
+
+  const ingredients = {
+    temperature: ["hot", "lukewarm", "cold"],
+    syrup: ["mocha", "vanilla", "toffee", "maple", "caramel", "other", "none"],
+    milk: ["cow", "oat", "goat", "almond", "none"],
+    blended: ["yes", "turbo", "no"],
+  };
+
   const onNewDrink = () => {};
 
   const onCheckAnswer = () => {};
@@ -21,6 +35,20 @@ const BaristaForm = () => {
       >
         New Drink
       </button>
+
+      <h3>Temperature</h3>
+      <div className="answer-space">{inputs["temperature"]}</div>
+      <RecipeChoices
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+          }))
+        }
+        label="temperature"
+        choices={ingredients["temperature"]}
+        checked={inputs["temperature"]}
+      />
     </div>
   );
 };
