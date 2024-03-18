d91b7fdabbfdb438ede56b593d46d4b62f03f4ac
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-03-18 14:11:33 -0400

    checkpoint 4 -- week 5 lab

--- cp-lab-5/src/components/CoinInfo.jsx
+++ cp-lab-5/src/components/CoinInfo.jsx
@@ -6,33 +6,34 @@ const CoinInfo = ({ image, name, symbol }) => {
     useEffect(() => {
         const getCoinPrice = async () => {
             const response = await fetch(
-              `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
+                `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=` +
                 API_KEY
             );
-          
+
             const json = await response.json();
             setPrice(json);
-          };
-          getCoinPrice().catch(console.error);
+        };
+        getCoinPrice().catch(console.error);
 
     }, [symbol]);
 
     return (
         <div>
-          {price ? ( // rendering only if API call actually returned us data
-            <li className="main-list" key={symbol}>
-            <img
-              className="icons"
-              src={`https://www.cryptocompare.com${image}`}
-              alt={`Small icon for ${name} crypto coin`}
-            />
-            {name} <span className="tab"></span> ${price.USD} USD
-          </li>
-          ) : 
-          null
-          }
-        </div>
+            {price ? ( // rendering only if API call actually returned us data
+                <li className="main-list" key={symbol}>
+                    <img
+                        className="icons"
+                        src={`https://www.cryptocompare.com${image}`}
+                        alt={`Small icon for ${name} crypto coin`}
+                    />
+                    {name} &lt;<span> className="tab" ${price.USD} USD </span>
+
+        </li>
+    ) :
+    null
+}
+        </div >
       );
   };
-  
-  export default CoinInfo;
+
+export default CoinInfo;
