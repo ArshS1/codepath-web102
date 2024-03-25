8c2b3030c0294bdd1d96bd1e7426433014b9f4b2
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-03-25 13:39:20 -0400

    checkpoint 4 -- week 6 lab

--- cp-lab-6/src/main.jsx
+++ cp-lab-6/src/main.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from './routes/Layout';
-import DetailView from './routes/DetailView';
+import Layout from "./routes/Layout";
+import DetailView from "./routes/DetailView";
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index={true} element={<App />} />
-    </Route>
-  </Routes>
-</BrowserRouter>
-)
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<App />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+            <Link style={{ color: "white" }} to="/">
+              Back to Home
+            </Link>
+          </main>
+        }
+      />{" "}
+    </Routes>
+  </BrowserRouter>
+);
