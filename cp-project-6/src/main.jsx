63319cad4eee3d10004f6c063c4d10fde71acd99
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2024-03-28 21:53:08 -0400

    finish all required features -- week 6 project

---
+++ cp-project-6/src/main.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./routes/Layout";
+import DetailView from "./routes/DetailView";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<App />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+            <Link to={`/detail/${list.Data[coin].Symbol}`}>
+  <CoinInfo
+    image={list.Data[coin].ImageUrl}
+    name={list.Data[coin].FullName}
+    symbol={list.Data[coin].Symbol}
+  />
+</Link>
+          </main>
+        }
+      />{" "}
+    </Routes>
+  </BrowserRouter>
+);
---
+++ cp-project-6/src/routes/DetailView.jsx
@@ -0,0 +1,19 @@
+import CoinDetail from "../Components/coinDetail";
+
+const DetailView = () => {
+  return (
+    <div>
+      <CoinDetail />
+      <LineChart width={500} height={300} data={coinData}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="name" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 8 }} />
+</LineChart>
+    </div>
+  );
+};
+
+export default DetailView;
